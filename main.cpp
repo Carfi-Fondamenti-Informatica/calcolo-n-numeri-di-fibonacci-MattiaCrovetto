dc51213a7f902c87ec9070ccce6eddede101e4ad
MattiaCrovetto <92729247+MattiaCrovetto@users.noreply.github.com>
2021-11-14 23:54:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+  int n=0, x1=0, x2=1, xf=x1+x2;
+          cin>>n;
+   if(n>=2){
+       cout<< x2 << endl;
+       for(int i=3; i<(n+1);++i){
+           cout<< xf << endl;
+           x1=x2;
+           x2=xf;
+           xf=x1+x2;
+       }
+   } else {
+       cout << "errore";
+   }
+    return 0;
 }
+
